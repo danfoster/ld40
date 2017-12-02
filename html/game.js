1fe04c72b2fc96eb1178f39a26c2600d45b54b6d
Dan Foster <dan@zem.org.uk>
2017-12-02 10:59:38 +0000

    Add Player sprite

--- html/game.js
+++ html/game.js
@@ -118112,15 +118112,43 @@ process.umask = function() { return 0; };
 
 
 class GameState extends __WEBPACK_IMPORTED_MODULE_0_phaser___default.a.State {
-	preload() {
-		this.level = new __WEBPACK_IMPORTED_MODULE_1_level__["a" /* default */]();
-	}
+  preload() {
+    window.game.load.image('player', 'assets/player.png');
 
-	create() {
-		window.game.stage.backgroundColor = '#440000';
-		this.level.createTilemap();
-	}
+    this.level = new __WEBPACK_IMPORTED_MODULE_1_level__["a" /* default */]();
+  }
 
+  create() {
+    window.game.stage.backgroundColor = '#442200';
+    this.level.createTilemap();
+
+    window.game.physics.startSystem(__WEBPACK_IMPORTED_MODULE_0_phaser___default.a.Physics.ARCADE);
+    this.player = window.game.add.sprite(10 * this.level.tilesize, 10 * this.level.tilesize, 'player');
+    window.game.physics.arcade.enable(this.player);
+
+    this.cursors = window.game.input.keyboard.createCursorKeys();
+  }
+
+  update() {
+    this.player.body.velocity.x = 0;
+    this.player.body.velocity.y = 0;
+
+    if (this.cursors.left.isDown) {
+      //  Move to the left
+      this.player.body.velocity.x = -150;
+    } else if (this.cursors.right.isDown) {
+      //  Move to the right
+      this.player.body.velocity.x = 150;
+    }
+
+    if (this.cursors.up.isDown) {
+      //  Move to the left
+      this.player.body.velocity.y = -150;
+    } else if (this.cursors.down.isDown) {
+      //  Move to the right
+      this.player.body.velocity.y = 150;
+    }
+  }
 }
 /* harmony export (immutable) */ __webpack_exports__["a"] = GameState;
 
--- src/states/game.js
+++ src/states/game.js
@@ -4,14 +4,54 @@ import Level from 'level';
 
 export default class GameState extends Phaser.State {
 	preload() {
+		window.game.load.image('player', 'assets/player.png');
+
 		this.level = new Level;
+		
 	}
 
 
 	create() {
-		window.game.stage.backgroundColor = '#440000';
+		window.game.stage.backgroundColor = '#442200';
 		this.level.createTilemap();
+
+    
+    window.game.physics.startSystem(Phaser.Physics.ARCADE);
+		this.player = window.game.add.sprite(
+        10*this.level.tilesize,
+        10*this.level.tilesize,
+        'player'
+    );
+    window.game.physics.arcade.enable(this.player);
+
+		this.cursors = window.game.input.keyboard.createCursorKeys();
+
 	}
 
+  update() {
+    this.player.body.velocity.x = 0;
+    this.player.body.velocity.y = 0;
+		
+		if (this.cursors.left.isDown)
+    {
+        //  Move to the left
+        this.player.body.velocity.x = -150;
+    }
+    else if (this.cursors.right.isDown)
+    {
+        //  Move to the right
+        this.player.body.velocity.x = 150;
+    }
 
+		if (this.cursors.up.isDown)
+    {
+        //  Move to the left
+        this.player.body.velocity.y = -150;
+    }
+    else if (this.cursors.down.isDown)
+    {
+        //  Move to the right
+        this.player.body.velocity.y = 150;
+    }
+  }
 }
